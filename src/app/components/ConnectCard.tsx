cf0215e12ddedb4a12170af4944bcc56d8285d50
Mohit Sharma <mohitsharma23j97@gmail.com>
2024-09-01 11:53:03 +0530

    feat: add ping animation

--- src/app/components/ConnectCard.tsx
+++ src/app/components/ConnectCard.tsx
@@ -5,7 +5,13 @@ export default function ConnectCard() {
     <Card className="lg:col-span-2 row-span-2 col-span-1">
       <div className="flex flex-col gap-4">
         <div className="font-bold text-2xl text-[#899878]">
-          Let&apos;s Connect
+          <div className="flex justify-between items-center">
+            <span>Let&apos;s Connect</span>
+            <span className="relative flex h-3 w-3">
+              <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-[#899878] opacity-75"></span>
+              <span className="relative inline-flex rounded-full h-3 w-3 bg-[#899878]"></span>
+            </span>
+          </div>
           <hr />
         </div>
         <div className="text-sm">
