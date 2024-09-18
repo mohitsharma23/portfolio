93db3c2c31109375e533148e07d80cb201969368
Mohit Sharma <mohitsharma23j97@gmail.com>
2024-09-18 22:02:02 +0530

    feat: added intro text

--- src/app/components/IntroCard.tsx
+++ src/app/components/IntroCard.tsx
@@ -13,11 +13,20 @@ export default function IntroCard() {
         <div className="flex flex-col justify-between h-full">
           <div>
             <div className="text-sm opacity-50">welcome</div>
-            <div>
-              Lorem ipsum dolor sit amet, consectetur adipisici elit ist ein
-              Blindtext, der nichts bedeuten soll, sondern als Platzhalter im
-              Layout verwendet wird, um einen Eindruck vom fertigen Schriftstück
-              zu erhalten
+            <div className="font-thin">
+              Hi, I&apos;m <span className="font-normal">Mohit</span>, a
+              front-end developer with expertise in crafting responsive,
+              user-friendly web interfaces with{" "}
+              <span className="font-normal">over 5 years of experience</span>. I
+              specialize in modern JavaScript frameworks like React and Angular,
+              delivering fast, dynamic, and accessible web applications. With a
+              focus on clean, maintainable code, I bring creativity and
+              precision to every project.
+              <br />
+              <br />
+              <span className="font-normal">
+                Let&apos;s build something amazing together!
+              </span>
             </div>
           </div>
           <div className="flex justify-end gap-4 mt-4">
